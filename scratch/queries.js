2527f57ca85c97464dbb5a260baa851e1bb16e37
MorganHuegel <mhue2910@gmail.com>
2018-06-26 15:43:26 -0400

    added scratch file, messed around with simDB methods

---
+++ scratch/queries.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+// GET Notes with search
+notes.filter('Dogs', (err, list) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(list);
+});
+
+// GET Notes by ID
+notes.find(1005, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// PUT (Update) Notes by ID
+const updateObj = {
+  title: 'New Title',
+  content: 'Blah blah blah'
+};
+
+notes.update(1005, updateObj, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+notes.create({title: 'bland', description: 'blah', id: 1010}, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+notes.delete(1001, (err) => {
+  if(err) {console.log(err)}
+  else {console.log(data)}
+});
